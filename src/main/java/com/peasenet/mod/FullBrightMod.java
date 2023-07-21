557fdf0950fa2d394b955d9c173fdbbc2dba7ec3
Gavin <gavinpease@gmail.com>
2023-07-21 12:24:52 -0600

    Remove logging, and optionally removed mixins file if required

--- src/main/java/com/peasenet/mod/FullBrightMod.java
+++ src/main/java/com/peasenet/mod/FullBrightMod.java
@@ -9,16 +9,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FullBrightMod implements ModInitializer {
-    public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-        LOGGER.info("Initializing GEM FullBright Mod...");
         Settings.addConfig("fullbright", new FullbrightConfig());
         GavinsMod.addMod(new ModFullBright());
-        LOGGER.info("GEM FullBright Mod Initialized!");
     }
 }
